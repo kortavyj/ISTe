5e491d3c04ccb8b18be4dc31e23ce6f8bc3dff80
Kortavyj <lapinjena323@gmail.com>
2026-08-01 00:23:00 +0300

    Update siteUrl.js

--- src/lib/siteUrl.js
+++ src/lib/siteUrl.js
@@ -1,1 +1,13 @@
+export function getSiteRootUrl() {
+  const path = window.location.pathname.endsWith("/")
+    ? window.location.pathname
+    : `${window.location.pathname}/`;
 
+  return `${window.location.origin}${path}`;
+}
+
+export function getAuthRedirectUrl(route) {
+  const normalizedRoute = route.startsWith("/") ? route : `/${route}`;
+
+  return `${getSiteRootUrl()}#${normalizedRoute}`;
+}
